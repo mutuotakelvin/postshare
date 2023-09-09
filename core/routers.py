e54fc8c5f433490f8e50b78d967c08045b9ff13f
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-09-09 16:54:31 +0300

    feat(app): update the routes add route for loging in

--- core/routers.py
+++ core/routers.py
@@ -1,11 +1,13 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
-from core.auth.viewsets.register import RegisterViewSet
+from core.auth.viewsets import RegisterViewSet,LoginViewSet
+
 
 router = routers.SimpleRouter()
 
 # auth
 router.register(r'auth/register',RegisterViewSet, basename='auth-register')
+router.register(r'auth/login',LoginViewSet, basename='auth-login')
 
 # USER
 router.register(r'user', UserViewSet, basename='user')
