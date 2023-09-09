187348bc056f233ee323510ec953257027c29434
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-09-09 16:06:27 +0300

    feat(app): register auth/register endpoint added a route for registering

--- core/routers.py
+++ core/routers.py
@@ -1,10 +1,15 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
+from core.auth.viewsets.register import RegisterViewSet
 
 router = routers.SimpleRouter()
 
+# auth
+router.register(r'auth/register',RegisterViewSet, basename='auth-register')
+
 # USER
 router.register(r'user', UserViewSet, basename='user')
+
 
 urlpatterns = [
     *router.urls,
