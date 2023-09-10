907ab61b8190c9cc43a5c372cf0f6867c3dd8aee
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-09-10 17:24:59 +0300

    feat(app): post viewsets add post viewsets

---
+++ core/post/viewsets.py
@@ -0,0 +1,31 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+from core.abstract.viewsets import AbstractViewSet
+from core.post.models import Post
+from core.post.serializers import PostSerializer
+
+class PostViewSet(AbstractViewSet):
+    hhtp_method_names = ('post','get')
+    permission_classes = (IsAuthenticated)
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return Post.objects.all()
+    
+    def get_object(self):
+        obj = Post.objects.get_object_by_public_id(
+            self.kwargs['pk']
+        )
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+    
+    def create(self,request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data,
+                        status=status.HTTP_201_CREATED)
